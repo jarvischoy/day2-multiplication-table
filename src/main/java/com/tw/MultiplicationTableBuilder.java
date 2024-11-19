238fdc45ec9e8e6423b0f8840b5fbea671e8c47d
Jarvis Choy <choyja2@oocl.com>
2024-11-19 11:52:03 +0800

    feat: add function to generate multiplication table

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -16,4 +16,20 @@ public class MultiplicationTableBuilder {
     public String generateExpression(int starter, int end) {
         return String.format("%d*%d=%d", starter, end, starter * end);
     }
+
+    public String generateTable(int starter, int end) {
+        StringBuilder multiplicationTable = new StringBuilder();
+
+        for (int i = starter; i <= end; i++) {
+            for (int j = starter; j <= i; j++) {
+                multiplicationTable.append(generateExpression(j, i));
+                if (j != i) {
+                    multiplicationTable.append(" ");
+                }
+            }
+            multiplicationTable.append(System.lineSeparator());
+        }
+
+        return multiplicationTable.toString();
+    }
 }
