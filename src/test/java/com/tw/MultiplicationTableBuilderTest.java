1ef5076ffc1df701938a12eb4ec4d4bcd074401c
Jarvis Choy <choyja2@oocl.com>
2024-11-19 10:36:58 +0800

    test: add should return false when isInRange given number 1001

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -31,5 +31,16 @@ class MultiplicationTableBuilderTest {
         assertTrue(!isInRange);
     }
 
-    // TODO: add more tests for isInRange
+    @Test
+    void should_return_false_when_isInRange_given_a_number_1001() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 1001;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertTrue(!isInRange);
+    }
 }
